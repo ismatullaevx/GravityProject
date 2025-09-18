cf5f24e91ded97ee1846090dba12d6be17152d81
ismatullaevx <tomicand6g@gmail.com>
2025-09-18 20:19:11 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
@@ -6,9 +7,13 @@ import Home from './pages/Home'
 function App() {
   return (
     <div className='bg-[#141619]'>
-      <Header />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/header' element={<Header />} />
+          <Route path='/footer' element={<Footer />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,19 @@
-
+// import lanier from '../assets/icons/lanier.png'
 import grafity from '../assets/imgs/grafity.png'
 
 export default function Header() {
-  return (
-    <div className='px-[60px] py-[40px]'>
-        <img src={grafity} alt="" />
-        <ul className=' w-full text-white'>
-            <li>About Us</li>
-            <li>Work with UsMas</li>
-            <select name="" id="">
-                <option value="">Our Services</option>
-            </select>
-            <li>Blog</li>
-        </ul>
-    </div>
-  )
+    return (
+        <div className='px-[60px] py-[40px] flex justify-between w-full'>
+            <img src={grafity} alt="" />
+            <ul className=' w-full text-white flex items-center'>
+                <li>About Us</li>
+                <select name="" id="">
+                    <option value="">Our Services</option>
+                </select>
+                <li>Work with Us</li>
+                <li>Blog</li>
+            </ul>
+          
+        </div>
+    )
 }
