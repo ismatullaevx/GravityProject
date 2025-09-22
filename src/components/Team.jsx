f0bf3b3c3bd970411342c76efc7ffb0153565abd
olimovfff <tvuzpubg07@gmail.com>
2025-09-22 16:13:15 +0500

    a

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -31,19 +31,20 @@ export default function Join() {
                     </div>
                 </div>
             </div>
-            <div >
+            <div>
                 <div
                     className={"IMgThree bg-no-repeat  container flex flex-col gap-10 items-center justify-center  min-h-screen w-screen"}>
 
 
                     <p className='text-white text-center text-[78px] '>Contact Us</p>
-                    <div className=' flex itemes-center justify-center'><p className='text-white text-center text-[18px] w-[40%]'>
+                    <div className=' flex itemes-center justify-center'><p
+                        className='text-white text-center text-[18px] w-[40%]'>
                         We are always open to discuss new value-adding partnerships. Do reach out if you are an exchange
                         or a project looking for liquidity; an algorithmic trader or a software developer looking to
                         improve the markets with us or just have a great idea you can’t wait to share with us!</p>
                     </div>
                     <div className=' flex itemes-center justify-center  '>
-                        <div className='leander2'>Get In Touch</div>
+                        <div className='Btn1 px-10 py-3'>GET IN TOUCH</div>
 
                     </div>
                 </div>
